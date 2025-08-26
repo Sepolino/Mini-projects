91cd151699343f7fbff99d93a12bab841cbd43cc
Marcus <viniszsx505@gmail.com>
2025-08-26 01:04:29 -0300

    Update Bank.java
    Fazendo do 0, adicionando o uso de HashMap

--- Bank/src/Bank.java
+++ Bank/src/Bank.java
@@ -1,56 +1,62 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 class Conta{
     private String nome;
+    private int password;
     private int id;
     private double saldo;
 
-    public Conta(String nome, int id, double saldo){
+    public Conta(String nome, int id){
         this.nome = nome;
         this.id = id;
+        this.password = password;
         this.saldo = saldo;
     }
 
-    public void depositar(double valor){
-        if(valor > 0){
+    public void Depositar(double valor){
+        if(valor <= 0){
+            System.out.println("Insira um valor maior que 0!");
+        }
+        else{
             saldo += valor;
-            System.out.println("Depósito feito com sucesso!");
-        }
-
-        else{
-            System.out.println("ERRO! Valor inválido. Faça um depósito de um valor maior que 0!");
+            System.out.println("Valor adicionado em seu saldo com sucesso!");
         }
     }
 
-    public void sacar(double valor){
-        if(valor > 0){
+    public void Saque(double valor){
+        if(valor <= 0){
+            System.out.println("Insira um valor maior que 0!");
+        }
+        else if(valor > saldo){
+            System.out.println("Saldo insuficiente!");
+            System.out.println("Saldo: R$" + saldo);
+        }
+        else{
             saldo -= valor;
-            System.out.println("Saque feito com sucesso!");
-        }
-        if(valor > saldo){
-            System.out.println("Saldo insuficiente! Tente outro valor..");
-        }
-        else{
-            System.out.println("ERRO! Valor inválido. Faça um saque de um valor maior que 0!");
+            System.out.println("Saque concluído com sucesso!");
         }
     }
 
-    public void transferir(Conta destino, double valor){
+    public void Transferir(Conta destino, double valor){
         if(destino == null){
-            System.out.println("ERRO! ID inválido ou conta inexistente.");
-        }
-        if(valor > saldo){
-            System.out.println("Saldo insuficiente! Tente outro valor..");
+            System.out.println("Conta não encontrada! Tente novamente.");
         }
         if(valor <= 0){
-            System.out.println("ERRO! Valor inválido. Faça uma transferência de um valor maior que 0!");
+            System.out.println("Insira um valor maior que 0!");
+        }
+        else if(valor > saldo){
+            System.out.println("Saldo insuficiente!");
+            System.out.println("Saldo: R$" + saldo);
         }
         else{
             saldo -= valor;
-            destino.depositar(valor);
-            System.out.println("Transferência realizada com sucesso!");
+            System.out.println("Saque concluído com sucesso!");
         }
+    }
+
+    public void exibirDados(){
+        System.out.printf("Nome: %s.10 | ID: %d.10 | Saldo: R$%f", nome, id, saldo);
     }
 
     public String getNome(){
@@ -65,159 +71,51 @@ class Conta{
         return saldo;
     }
 
-    public void exibirDados(){
-        System.out.println("Nome: " + nome + ", ID: " + id + ", Saldo: " + saldo);
+    public int getPsw(){
+        return password;
     }
+
 }
 
 class Banco{
-    private ArrayList<Conta> contas;
+    private HashMap<Integer, Conta> contas;
+    private int nmrId = 1;
 
     public Banco(){
-        contas = new ArrayList<>();
+        contas = new HashMap<>();
     }
-
-    public Conta criarConta(String nome, int id, double saldo){
-        Conta novaConta = new Conta(nome, id , saldo);
-        contas.add(novaConta);
-        System.out.println("Conta criada com sucesso! ID da conta: " + id);
+    public Conta criarConta(String nome, int passw, double saldo){
+        Conta novaConta = new Conta(nome, nmrId);
+        nmrId++;
+        System.out.println("Conta criada com sucesso! ID/Número da conta: " + novaConta.getId());
         return novaConta;
     }
 
-    public Conta buscarConta(int id){
-        for(Conta conta : contas){
-            if(conta.getId() == id){
-                return conta;
-            }
+    public Conta acessarConta(int id){
+        return contas.get(id);
+    }
+
+    public Conta mudarSenha(int password1, int password2){
+        if(password1 != password2){
+            System.out.println("As senhas digitadas não estão iguais, digite novamente!");
         }
-        return null;
+        else{
+            System.out.println("Senha alterada com sucesso!");
+        }
+
     }
 
     public void lista(){
         if(contas.isEmpty()){
-            System.out.println("Não existe nenhuma conta bancária registrada..");
+            System.out.println("Não existe nenhuma conta criada!");
+            return;
         }
         else{
-            for(Conta conta : contas){
+            for (Conta conta: contas.values()){
                 conta.exibirDados();
             }
         }
     }
 
 
-}
-
-public class Bank{
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        Banco banco = new Banco();
-        byte op = 0;
-
-        while(op != 9){
-            System.out.println("================BankOS 1.0=============");
-            System.out.println("1. Criar uma nova conta");
-            System.out.println("2. Depositar");
-            System.out.println("3. Sacar");
-            System.out.println("4. Transferir saldo");
-            System.out.println("5. Consultar conta");
-            System.out.println("6. Exibir uma lista de contas");
-            System.out.println("9. Sair");
-            op = Byte.parseByte(scanner.nextLine());
-
-            switch(op){
-                case 1 :
-                    System.out.println("==============Nova conta=============");
-                    System.out.print("Digite um nome de usuário: ");
-                    String nomeCriar = scanner.nextLine();
-
-                    System.out.print("Digite um ID: ");
-                    int idCriar = Integer.parseInt(scanner.nextLine());
-
-                    System.out.print("Insira o saldo inicial: ");
-                    double saldoCriar = Double.parseDouble(scanner.nextLine());
-
-                    banco.criarConta(nomeCriar, idCriar, saldoCriar);
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-                case 2 :
-                    System.out.println("==============Depositar na conta==========");
-                    System.out.print("Digite o ID da conta que você quer depositar: ");
-                    int idDep = Integer.parseInt(scanner.nextLine());
-
-                    System.out.print("Digite o valor que você quer depositar: ");
-                    double valorDep = Double.parseDouble(scanner.nextLine());
-
-                    Conta cDep = banco.buscarConta(idDep);
-                    if(cDep != null ) cDep.depositar(valorDep);
-                    else System.out.println("Conta não encontrada. Insira um ID válido!");
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-                case 3:
-                    System.out.println("===============Saque===============");
-                    System.out.print("Digite o ID da conta que você quer sacar: ");
-                    int idSaq = Integer.parseInt(scanner.nextLine());
-
-                    System.out.print("Digite o valor que você quer sacar: ");
-                    double valorSaq = Double.parseDouble(scanner.nextLine());
-
-                    Conta cSaq = banco.buscarConta(idSaq);
-                    if(cSaq != null) cSaq.depositar(valorSaq);
-                    else System.out.println("Conta não encontrada. Insira um ID válido!");
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-                case 4:
-                    System.out.println("==============Transferência============");
-                    System.out.print("Digite o ID da conta que você quer sacar para transferência: ");
-                    int idTrf1 = Integer.parseInt(scanner.nextLine());
-
-                    System.out.print("Digite o valor que você quer sacar: ");
-                    double valorTrf = Double.parseDouble(scanner.nextLine());
-
-                    System.out.print("Digite o ID da conta que você realizar a transferência: ");
-                    int idTrf2 = Integer.parseInt(scanner.nextLine());
-
-                    Conta cTrf1 = banco.buscarConta(idTrf1);
-                    Conta cTrf2 = banco.buscarConta(idTrf2);
-                    if(cTrf1 != null && cTrf2 != null){
-                        cTrf1.transferir(cTrf2, valorTrf);
-                    }
-                    else{
-                        System.out.println("Conta não encontrada. Digite um ID válido.");
-                    }
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-
-                case 5:
-                    System.out.println("=================Consultaweb============");
-                    System.out.print("Digite o ID para encontrar/consultar a conta: ");
-                    int idCon = Integer.parseInt(scanner.nextLine());
-
-                    Conta cCon = banco.buscarConta(idCon);
-                    if(cCon == null){
-                        System.out.println("===========================");
-                        System.out.println("Nome: " + cCon.getNome());
-                        System.out.println("ID: " + cCon.getId());
-                        System.out.println("Saldo: " + cCon.getSaldo());
-                        System.out.println("Pressione Enter para continuar...");
-                        scanner.nextLine();
-                    }
-                    else{
-                        System.out.println("Conta não encontrada! Tente novamente...");
-                    }
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-                case 6:
-                    System.out.println("==================Lista=================");
-                    banco.lista();
-                    System.out.println("Pressione Enter para continuar...");
-                    scanner.nextLine();
-                    break;
-            }
-        }
-    }
 }
